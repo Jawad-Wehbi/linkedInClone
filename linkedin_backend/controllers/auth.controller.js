054fde30380912a2d7a2f846e90328620ee4fa69
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-10-16 17:46:52 +0300

    Backend<>Cleaning Code

--- linkedin_backend/controllers/auth.controller.js
+++ linkedin_backend/controllers/auth.controller.js
@@ -1,43 +1,45 @@
-const User = require('../models/User.model');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require('../models/User.model')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-const login = async (req, res)=>{
-    const {email, password} = req.body;
-    
-    const user = await User.findOne({email}).select("+password");
+const login = async (req, res) => {
+  const { email, password } = req.body
 
-    if(!user) return res.status(404).json({message: "Invalid Credentials"});
+  const user = await User.findOne({ email }).select('+password')
 
-    const isMatch = bcrypt.compare(password, user.password);
-    if(!isMatch) return res.status(404).json({message: "Invalid Credentials"});
+  if (!user) return res.status(404).json({ message: 'Invalid Credentials' })
 
-    const token = jwt.sign({email: user.email, name: user.name, userType: User.user_type}, process.env.JWT_SECRET_KEY, {
-        expiresIn: '1h'
-    });
-    res.status(200).json(token)
-    
+  const isMatch = bcrypt.compare(password, user.password)
+  if (!isMatch) return res.status(404).json({ message: 'Invalid Credentials' })
+
+  const token = jwt.sign(
+    { email: user.email, name: user.name, userType: User.user_type },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: '1h',
+    },
+  )
+  res.status(200).json(token)
 }
-const signup = async (req, res)=>{
-    const {name, email, password, user_type, phone_number} = req.body;
-    try{
-        const user = new User();
-        user.name = name;
-        user.email = email;
-        user.password = await bcrypt.hash(password, 10);
-        user.user_type = user_type;
-        user.phone_number = phone_number;
+const signup = async (req, res) => {
+  const { name, email, password, user_type, phone_number } = req.body
+  try {
+    const user = new User()
+    user.name = name
+    user.email = email
+    user.password = await bcrypt.hash(password, 10)
+    user.user_type = user_type
+    user.phone_number = phone_number
 
-
-        await user.save();
-        res.json(user)
-    }catch(err){
-        res.status(400).json({
-            message: err.message
-        })
-    }
+    await user.save()
+    res.json(user)
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    })
+  }
 }
 module.exports = {
-    login,
-    signup
+  login,
+  signup,
 }
--- linkedin_backend/controllers/company.controller.js
+++ linkedin_backend/controllers/company.controller.js
@@ -1,28 +1,35 @@
 const Job = require('../models/Job.model')
 const User = require('../models/User.model')
 
-const getAllJobs = async (req, res) => {
-  const job = req.job
-  res.json({ job: Job })
-}
+
 
 const updateProfile = async (req, res) => {
-  params = req.body;
+  params = req.body
   const user = await User.updateOne(
     { email: params.email },
-    { name: params.name, 
-      phone_number: params.phone_number, 
-      profile_photo: params.profile_photo, 
-      bio:params.bio, 
-      country:params.country, 
-      city:params.city,
-      number_of_employees:params.number_of_employees },
+    {
+      name: params.name,
+      phone_number: params.phone_number,
+      profile_photo: params.profile_photo,
+      bio: params.bio,
+      country: params.country,
+      city: params.city,
+      number_of_employees: params.number_of_employees,
+    },
   )
   res.json(user)
 }
-
-
+const jobAdd= async (req, res) =>{
+  params = req.body
+  const job = new Job({
+    job_title: params.job_title,
+    job_description: params.job_description,
+    major: params.major,
+    company_id: params.company_id,
+    applicants: {},
+  })
+}
 module.exports = {
-  getAllJobs,
-  updateProfile
+  updateProfile,
+  jobAdd
 }
