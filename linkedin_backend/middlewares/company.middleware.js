6ed3fc8590783a664984ccf173dfcfa746ac5e8c
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-10-16 19:21:49 +0300

    Backend<>Fixing middlewares

--- linkedin_backend/middlewares/company.middleware.js
+++ linkedin_backend/middlewares/company.middleware.js
@@ -1,18 +1,18 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/User.model')
+const jwt = require('jsonwebtoken');
+const User = require('../models/User.model');
 
-const authMiddleware = async (req, res, next) => {
+const companyMiddleware = async (req, res, next) => {
   const token = req.headers.authorization.split(' ')[1]
 
   if (!token) return res.status(401).json({ message: '1 Unauthorized' })
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
     const user = await User.findOne({ email: decoded.email }).lean()
-    if (user) next()
+    if (user && decoded.user_type === 'Company') next()
   } catch (err) {
     console.log('err :>> ', err)
     res.status(401).json({ message: '2 Unauthorized' })
   }
 }
 
-module.exports = authMiddleware
+module.exports = companyMiddleware
