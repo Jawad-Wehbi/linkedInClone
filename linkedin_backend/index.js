0bb1be51a22f844178dbc81dc8da487af166d9f1
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-10-16 16:08:09 +0300

    Backend<> Index file update

--- linkedin_backend/index.js
+++ linkedin_backend/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 require('dotenv').config();
 require('./config/db.config')
 const authRoutes = require('./routes/auth.routes');
+const userRoutes = require('./routes/users.routes');
 
 
 const app = express();
 app.use(express.json());
 
 app.use('/auth', authRoutes)
+app.use('/user', userRoutes)
 
 app.listen(process.env.PORT, (err) => {
   if(err) throw err;
